dbea4df7b5947749d98fbf48e700c803a7b5cdac
Fabien LOISON <flo@flogisoft.com>
2018-10-15 17:50:05 +0200

    v1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if os.path.isfile("README.rst"):
 
 setup(
     name="saltimbanque",
-    version="1.0.1",
+    version="1.1.0",
     description="Provides an API to convert web pages to PDF",
 
     long_description=long_description,
