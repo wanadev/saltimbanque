9e49bbd368ad4bd9885b90eb0e9afb050d9ddb8a
Fabien LOISON <flo@flogisoft.com>
2018-10-15 17:49:41 +0200

    Encode url to base64, adds index to check

--- saltimbanque/app.py
+++ saltimbanque/app.py
@@ -1,3 +1,5 @@
+from base64 import b64decode
+
 from flask import Flask, Response
 
 from .pdf import pdf_from_url
@@ -6,8 +8,30 @@ from .pdf import pdf_from_url
 app = Flask(__name__)
 
 
-@app.route("/html-to-pdf/<path:url>")
+@app.route("/")
+def index():
+    return """
+    <html>
+        <head>
+            <title>Saltimbanque</title>
+        </head>
+        <body>
+        <input type="text" id="url" />
+        <button onclick="convert()">Convert to PDF!</button>
+        <script>
+            function convert() {
+                var url = document.getElementById('url').value;
+                window.location = '/html-to-pdf/' + btoa(url);
+            }
+        </script>
+        </body>
+    </html>
+    """  # noqa
+
+
+@app.route("/html-to-pdf/<string:url>")
 def html_to_pdf(url):
+    url = b64decode(url)
     resp = Response(pdf_from_url(url))
     resp.headers["Content-Type"] = "application/pdf"
     return resp
